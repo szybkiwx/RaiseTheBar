bc2d8c4160203337e220f2bf354239f407326c51
szybkiwx <andrzej.dajnowski@gmail.com>
2016-06-04 21:29:48 +0200

    adding console runner

---
+++ RaiseTheBar.ConsoleRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using RiseTheBar;
+using System;
+
+
+namespace RaiseTheBar.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:32056"))
+            {
+                Console.WriteLine("Listening on http://localhost:3205.");
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
